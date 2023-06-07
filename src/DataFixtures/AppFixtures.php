d193553aeb6e2233df47fd3c7364d9e10edd30c3
Dev1 <dev1@web.de>
2023-06-07 07:54:29 +0200

    Saving Relations
    symfony console doctrine:fixtures:load
    #if needed
    symfony console doctrine:migrations:migrate
    
    symfony console doctrine:fixtures:load
    
    symfony console doctrine:query:sql 'SELECT * FROM answer'
    
    symfony console doctrine:query:sql 'SELECT * FROM question WHERE id = 103'

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -16,8 +17,23 @@ class AppFixtures extends Fixture
         QuestionFactory::new()
             ->unpublished()
             ->many(5)
-            ->create()
-        ;
+            ->create();
+
+        $answer = new Answer();
+        $answer->setContent(
+            'This question is the best? I wish... I knew the answer.'
+        );
+        $answer->setUsername('weaverryan');
+        $question = new Question();
+        $question->setName('How to un-disappear your wallet.');
+        $question->setQuestion('... I should not have done this...');
+        //setting relation between answer and question-entity by passing the question-object itself (question-record will be referenced via the fk question_id from the answer-table)
+        //Doctrine is smart enough to save first question-entity and use its question_id as fk for answer-table
+
+        $answer->setQuestion($question);
+        $manager->persist($answer);
+        $manager->persist($question);
+
 
         $manager->flush();
     }
