4042cbcc5dfe589c5adf6642b7373e39074947a0
Dev1 <dev1@web.de>
2023-06-23 11:20:30 +0200

    Handling ManyToMany in Foundry symfony console make:factory symfony console doctrine:fixtures:load symfony console doctrine:query:sql 'SELECT * FROM tag' symfony console doctrine:query:sql 'SELECT * FROM question_tag'

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\Question;
 use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
+use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,11 +15,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $questions = QuestionFactory::createMany(20);
+        TagFactory::createMany(100);
+        $questions = QuestionFactory::createMany(20, function () {
+            return [
+                'tags' => TagFactory::randomRange(0, 5),
+            ];
+        });
 
         QuestionFactory::new() //get a second instance of question-factory
-            ->unpublished() //->unpublished() to change the default askedAt data.  create a new factory, make everything unpublished and create 5.
-            ->many(5)
+        ->unpublished(
+        ) //->unpublished() to change the default askedAt data.  create a new factory, make everything unpublished and create 5.
+        ->many(5)
             ->create();
 
         //classic way without foundry
@@ -47,41 +54,41 @@ class AppFixtures extends Fixture
          }); */
 
         //randomly grab only one of the 20 published questions and assign them to answers.
-        AnswerFactory::createMany(100, function() use ($questions) {
+        AnswerFactory::createMany(100, function () use ($questions) {
             return [
-                'question' => $questions[array_rand($questions)]
+                'question' => $questions[array_rand($questions)],
             ];
         });
 
-       /* $question = QuestionFactory::createOne();
-        $answer1 = new Answer();
-        $answer1->setContent('answer 1');
-        $answer1->setUsername('weaverryan');
-        $answer2 = new Answer();
-        $answer2->setContent('answer 2');
-        $answer2->setUsername('weaverryan1');
-        $question->addAnswer($answer1);
-        $question->addAnswer($answer2);
+        /* $question = QuestionFactory::createOne();
+         $answer1 = new Answer();
+         $answer1->setContent('answer 1');
+         $answer1->setUsername('weaverryan');
+         $answer2 = new Answer();
+         $answer2->setContent('answer 2');
+         $answer2->setUsername('weaverryan1');
+         $question->addAnswer($answer1);
+         $question->addAnswer($answer2);
 
-        $manager->persist($answer1);
-        $manager->persist($answer2);*/
+         $manager->persist($answer1);
+         $manager->persist($answer2);*/
 
         //So this will create 20 new, "needs approval" answers that are set to a random published Question
         AnswerFactory::new(function () use ($questions) {
             return [
-                'question' => $questions[array_rand($questions)]
+                'question' => $questions[array_rand($questions)],
             ];
-        } )->needsApproval()->many(20)->create();
+        })->needsApproval()->many(20)->create();
 
         //Fix for:   App\Entity\Tag::addQuestion(): Argument #1 ($question) must be of type App\Entity\Question, Zenstruck\Foundry\Proxy given, called in /var/www/project/src/
         //  DataFixtures/AppFixtures.php on line 87  ->object() -> now question is a pure question-object (no foundry-proxy)
-        $question = QuestionFactory::createOne()->object();
-        $tag1 = new Tag();
-        $tag1->setName('dinosaurs');
-        $tag2 = new Tag();
-        $tag2->setName('monster trucks');
-        $question->addTag($tag1); //you can only set in relation via the owning side
-        $question->addTag($tag2); //you can only set in relation via the owning side
+        /*  $question = QuestionFactory::createOne()->object();
+          $tag1 = new Tag();
+          $tag1->setName('dinosaurs');
+          $tag2 = new Tag();
+          $tag2->setName('monster trucks');
+          $question->addTag($tag1); //you can only set in relation via the owning side
+          $question->addTag($tag2); //you can only set in relation via the owning side*/
 
         //experiment, that setting objects in relation via inversed-side does not work
         //you can only set in relation via the owning side
@@ -90,12 +97,11 @@ class AppFixtures extends Fixture
 //        $tag1->addQuestion($question);
 //        $tag2->addQuestion($question);
 
-        $manager->persist($tag1);
-        $manager->persist($tag2);
+//        $manager->persist($tag1);
+//        $manager->persist($tag2);
 
 
         $manager->flush();
-
 //        $question->removeTag($tag1);
     }
 }
