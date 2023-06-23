aeb2753974916c560899669739cc88a68739cbc5
Dev1 <dev1@web.de>
2023-06-23 11:51:25 +0200

    Joining Across a ManyToMany

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -27,6 +27,19 @@ class QuestionRepository extends ServiceEntityRepository
     {
         return $this->addIsAskedQueryBuilder()
             ->orderBy('q.askedAt', 'DESC')
+            /**
+             * To fix the N+1 problem, we need to add a join. And this is where things get interesting. In the database,
+             * we need to join from question to question_tag... and then join from question_tag over to tag. So we actually need two joins.
+             * But in Doctrine, we get to pretend like that join table doesn't exist:
+             * Doctrine wants us to pretend that there is a direct relationship from question to tag.
+             * What I mean is, to do the join, all we need is ->leftJoin() - because we want to get the many tags for this question - q.tags, tag.
+             * That's it. We reference the tags property on question... and let Doctrine figure out how to join over to that.
+             * The second argument - tag - becomes the alias to the data on the tag table. We need that to select its data: addSelect('tag').
+             * So... yup! Joining across a ManyToMany relationship is no different than joining across a ManyToOne relationship:
+             * you reference the relation property (q.tags) and Doctrine does the heavy lifting.
+             */
+            ->leftJoin('q.tags', 'tag')
+            ->addSelect('tag')
             ->getQuery()
             ->getResult()
         ;
